927533babd06f9e02ce2b946584b12e740db8a9f
Lee Doha <79359308+leedoha11@users.noreply.github.com>
2025-01-04 15:18:54 +0900

    feature: 파일명 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WelcomePage from '@/pages/WelcomePage.vue'
-import InputName from '@/pages/InputName.vue'
-import SurveyStep1 from '@/pages/SurveyStep1.vue'
-import SurveyStep2 from '@/pages/SurveyStep2.vue'
-import SurveyStep3 from '@/pages/SurveyStep3.vue'
-import ResultPage from '@/pages/ResultPage.vue'
+import PageOne from '@/pages/PageOne.vue'
+import PageTwo from '@/pages/PageTwo.vue'
+import PageThree from '@/pages/PageThree.vue'
+import PageFour from '@/pages/PageFour.vue'
+import PageFive from '@/pages/PageFive.vue'
+import PageSix from '@/pages/PageSix.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'welcomePage',
-      component: WelcomePage,
+      path: '/page-1',
+      name: 'page-1',
+      component: PageOne,
     }
     ,
     {
-      path: '/input-name',
-      name: 'inputName',
-      component: InputName,
+      path: '/page-2',
+      name: 'page-2',
+      component: PageTwo,
     }
     ,
     {
-      path: '/survey-step1',
-      name: 'survey-step1',
-      component: SurveyStep1,
+      path: '/page-3',
+      name: 'page-3',
+      component: PageThree,
     }
     ,
     {
-      path: '/survey-step2',
-      name: 'survey-step2',
-      component: SurveyStep2,
+      path: '/page-4',
+      name: 'page-4',
+      component: PageFour,
     }
     ,
     {
-      path: '/survey-step3',
-      name: 'survey-step3',
-      component: SurveyStep3,
+      path: '/page-5',
+      name: 'page-5',
+      component: PageFive,
     }
     ,
     {
-      path: '/result',
-      name: 'resultPage',
-      component: ResultPage,
+      path: '/page-6',
+      name: 'page-6',
+      component: PageSix,
     }
   ],
 })
