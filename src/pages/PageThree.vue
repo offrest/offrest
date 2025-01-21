cf336b8ed1f8b2f0e99b3b9fb5c5142cb07ccf01
Lee Doha <79359308+leedoha11@users.noreply.github.com>
2025-01-21 20:48:52 +0900

    feat: 주석삭제

--- src/pages/PageThree.vue
+++ src/pages/PageThree.vue
@@ -6,7 +6,6 @@ import questionData from '@/data/questions.json';
 const route = useRoute(); 
 const router = useRouter(); 
 
-//const selectedOption = ref(null);
 const questions = ref(questionData);
 const currentQuestion = ref({});
 const selectedAnswers = ref([]);
@@ -17,25 +16,15 @@ const updateQuestion = () => {
   currentQuestion.value = question;
 };
 
-// URL 파라미터 변경을 감지
 watch(() => route.params.id, updateQuestion, { immediate: true });
 
 const selectOption = (option) => {
   const id = parseInt(route.params.id, 10);
-
   const question = questions.value.find(q => q.id === id);
 
-  // 선택한 옵션이 A인지 B인지 확인
   const selectedKey = question.A === option ? 'A' : 'B';
-
-  // 해당 질문 ID에 A 또는 B 저장
   selectedAnswers.value[id - 1] = selectedKey;
-
-  // LocalStorage에 저장
   localStorage.setItem('answers', JSON.stringify(selectedAnswers.value));
-  // // 선택한 답안을 저장
-  // selectedAnswers.value[id - 1] = option;
-  // localStorage.setItem('answers', JSON.stringify(selectedAnswers.value));
 
   const nextId = id + 1;
   if (questions.value.some(q => q.id === nextId)) {
