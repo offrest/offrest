0733b5503ab257455356fd0136c35171d3b50b0b
Lee Doha <79359308+leedoha11@users.noreply.github.com>
2025-02-02 11:23:19 +0900

    test: 배포 test

--- src/pages/PageOne.vue
+++ src/pages/PageOne.vue
@@ -22,7 +22,7 @@ onMounted(() => {
       </header>
 
       <div class="content">
-        <p class="title title--l">이 음음음악이 당신에게<br class="mo-only"/> 휴식이 되었으면 좋겠습니다.</p>
+        <p class="title title--l">이 음악이 당신에게<br class="mo-only"/> 휴식이 되었으면 좋겠습니다.</p>
 
         <div class="obj">
           <img src="../assets/images/obj.jpg" alt="음표" />
